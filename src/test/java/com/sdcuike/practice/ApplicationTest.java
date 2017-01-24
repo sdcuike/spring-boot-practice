c6dfffd5a306d8fe39b2732cfcd611619eedf2a8
sdcuike <sdcuike@aol.com>
2017-01-24 14:16:42 +0800

    更新jar依赖

---
+++ src/test/java/com/sdcuike/practice/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.sdcuike.practice;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
