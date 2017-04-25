d1c75bb80ca4b6cbe4e4dd5264246e5aaee2499d
sdcuike <sdcuike@aol.com>
2017-04-25 16:40:00 +0800

    修改mapper文件，删除重复自动生成的，由于注解去掉了，要自动生成一次，以后不能生成

--- src/main/java/com/sdcuike/practice/domain/City.java
+++ src/main/java/com/sdcuike/practice/domain/City.java
@@ -3,25 +3,16 @@ package com.sdcuike.practice.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.io.Serializable;
 
-@Entity
 @Data
 @NoArgsConstructor
 public class City implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue
     private long              id;
 
-    @Column(nullable = false)
     private String            name;
 
-    @Column(nullable = false)
     private String            state;
 }
