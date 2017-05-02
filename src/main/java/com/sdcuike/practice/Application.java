6da81e4c9e15f584529414f305d53a73b65250d6
sdcuike <sdcuike@aol.com>
2017-05-02 14:49:38 +0800

    服务启动打印服务日志配置信息

--- src/main/java/com/sdcuike/practice/Application.java
+++ src/main/java/com/sdcuike/practice/Application.java
@@ -1,11 +1,16 @@
 package com.sdcuike.practice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author sdcuike
@@ -23,9 +28,34 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
                 HibernateJpaAutoConfiguration.class},
         scanBasePackages = {"com.sdcuike.practice",
                 "com.sdcuike.spring.controller.advice"})
+@Slf4j
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.run(args);
+        ConfigurableEnvironment env = springApplication.run(args).getEnvironment();
+        
+        printInfo(env);
     }
+    
+    private static void printInfo(ConfigurableEnvironment env) throws UnknownHostException {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n\t" +
+                        "server contextPath:\t{} \n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.contextPath"),
+                env.getActiveProfiles());
+    }
+    
 }
