9b35a50cbe66044c789eb55aac67fcebb4c37701
sdcuike <sdcuike@aol.com>
2017-05-14 17:41:44 +0800

    spring-boot-oauth-authorization-server demo

--- src/test/java/com/sdcuike/practice/ApplicationTest.java
+++ src/test/java/com/sdcuike/practice/ApplicationTest.java
@@ -3,6 +3,7 @@ package com.sdcuike.practice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -11,7 +12,9 @@ public class ApplicationTest {
 
     @Test
     public void test() {
-    
+        BCryptPasswordEncoder pw = new BCryptPasswordEncoder();
+        String system = pw.encode("user");
+        System.out.println(system);
     }
     
 }
